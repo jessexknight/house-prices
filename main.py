ecfdf69555a353cdfb6fbc3483d17ed6b0a64fd4
= <=>
2018-01-10 17:11:54 -0500

    exploring optimizers, architectures

--- main.py
+++ main.py
@@ -34,11 +34,11 @@ def get_data(datafile,loady=True):
 def init_weights(shape):
   return tf.Variable(tf.random_normal(shape, stddev=np.sqrt(2.0/shape[0])))
 
-def build_model(X, W):
+def build_model(X, W, dropout=0):
   h = X
   for wi in W[:-1]:
-    h = tf.nn.relu(tf.matmul(h, wi))
-  return tf.matmul(h,W[-1])
+    h = tf.nn.dropout(tf.nn.relu(tf.matmul(h, wi)),1.0-dropout)
+  return tf.nn.relu(tf.matmul(h,W[-1]))
 
 # initialization
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
@@ -48,12 +48,12 @@ tf.reset_default_graph()
 # load the data
 trX,trY,trId = get_data('data/train.csv')
 teX,teY,teId = get_data('data/test.csv',loady=False)
-trX,vaX,trY,vaY,trId,vaId = tsplit(trX,trY,trId,test_size=0.25)
+trX,vaX,trY,vaY,trId,vaId = tsplit(trX,trY,trId,test_size=0.3)
 
 # define the parameters
 size_in  = trX.shape[1]
 size_out = trY.shape[1]
-size_h   = [size_in,256,128,size_out]
+size_h   = [size_in,512,64,size_out]
 batch_size = 128
 
 # define tf symbolic variables
@@ -62,12 +62,21 @@ Y  = tf.placeholder("float", [None, size_out])
 W  = [init_weights([hi,ho]) for hi,ho in zip(size_h[:-1],size_h[1:])]
 
 # define the model & operations
-Yp         = build_model(X, W)
-objective  = tf.reduce_mean(tf.pow(Yp-Y, 2.))
-optimizer  = tf.train.ProximalAdagradOptimizer(
-                learning_rate = 0.001,
-                l1_regularization_strength = 0.01,
-                l2_regularization_strength = 0.01)
+Yt         = build_model(X, W, dropout=0.3)
+Yp         = build_model(X, W, dropout=0.0)
+objective  = tf.reduce_mean(tf.pow(Yt-Y, 2.))
+Jfun       = tf.reduce_mean(tf.pow(Yp-Y, 2.))
+optimizer  = tf.train.AdagradOptimizer(
+                  learning_rate             = 0.1,
+                  initial_accumulator_value = 0.1)
+# optimizer  = tf.train.RMSPropOptimizer(
+#                 learning_rate = 0.001,
+#                 decay         = 0.9,
+#                 momentum      = 0.1)
+# optimizer  = tf.train.ProximalAdagradOptimizer(
+#                 learning_rate = 0.05,
+#                 l1_regularization_strength = 0.05,
+#                 l2_regularization_strength = 0.01)
 train_op   = optimizer.minimize(objective)
 predict_op = Yp
 
@@ -75,7 +84,7 @@ predict_op = Yp
 writer = {}
 writer.update({'tr':tf.summary.FileWriter('./log/tr')})
 writer.update({'va':tf.summary.FileWriter('./log/va')})
-tf.summary.scalar('Objective',  objective)
+tf.summary.scalar('Objective',  Jfun)
 summary = tf.summary.merge_all()
 
 # Launch the graph in a session
@@ -90,7 +99,8 @@ with tf.Session() as sess:
     writer['tr'].flush()
     writer['va'].add_summary(sess.run(summary,feed_dict={X:vaX,Y:vaY}),e)
     writer['va'].flush()
-    print str(e).rjust(5)
+    if not e%100:
+      print str(e).rjust(5)
 
   # predict the test data
   predictions = sess.run(predict_op, feed_dict={X: teX})
